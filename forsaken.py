b859033d7eedc96b1e977b1728c02be4b55d07a9
Bob <jaykmalhbergramolete@gmail.com>
2026-05-11 13:00:39 +0800

    Playing with lines

--- forsaken.py
+++ forsaken.py
@@ -6,11 +6,19 @@ screen = pygame.display.set_mode((600, 600))
 clock = pygame.time.Clock()
 running = True
 
-CELL_SIZE: int = 50
+CELL_SIZE: int = 100
+OFFSET: int = 0 # NOTE For later
 moveX: int = 0 # NOTE This will be used to keep track of where the line is going
 moveY: int = 0
 score: int = 0
 highscore: int = 0
+
+
+pygame.draw.line(screen, 'white', (100,0), (100,600))
+
+pygame.draw.lines(screen, 'white', closed=False, points=[(50,50), (100,100), (200, 50)])
+
+
 
 while running:
 
