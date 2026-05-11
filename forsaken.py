8f3a1becf403b18b9e8fe29770e61a7476d26f13
Bob <jaykmalhbergramolete@gmail.com>
2026-05-11 13:37:13 +0800

    Basic Grid Setup

--- forsaken.py
+++ forsaken.py
@@ -6,6 +6,12 @@ screen = pygame.display.set_mode((600, 600))
 clock = pygame.time.Clock()
 running = True
 
+font = pygame.font.Font(None, 25)
+
+# ======================
+# GAME VARIABLES
+# ======================
+
 CELL_SIZE: int = 100
 OFFSET: int = 0 # NOTE For later
 moveX: int = 0 # NOTE This will be used to keep track of where the line is going
@@ -13,12 +19,40 @@ moveY: int = 0
 score: int = 0
 highscore: int = 0
 
+# ======================
+# STARTUP FUNCTIONS
+# ======================
 
-pygame.draw.line(screen, 'white', (100,0), (100,600))
+# GRID SETUP
+# NOTE Maybe add a boarder later
+for x in range(1, 7):
+    pygame.draw.line(screen, 'white', (x*CELL_SIZE,0), (x*CELL_SIZE,600), width=2)
+for y in range(1, 7):
+    pygame.draw.line(screen, 'white', (0,y*CELL_SIZE), (600,y*CELL_SIZE), width=2)
+        
+# DEBUG: Grid Points
+for x in range(6):
+    for y in range(6):
+        pygame.draw.circle(screen, 'white', (50+x*CELL_SIZE,50+y*CELL_SIZE), radius=40)
+        text = font.render(f"{random.randint(1, 101)}", True, 'black')
+        text_rect = text.get_rect(center=(50+x*CELL_SIZE,50+y*CELL_SIZE))
+        screen.blit(text, text_rect)
 
-pygame.draw.lines(screen, 'white', closed=False, points=[(50,50), (100,100), (200, 50)])
+# ======================
+# GAME FUNCTIONS
+# ======================
 
 
+
+# ======================
+# STARTUP GAME
+# ======================
+
+
+
+# ======================
+# MAIN GAMEPLAY LOOP
+# ======================
 
 while running:
 
